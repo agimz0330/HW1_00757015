0cae24bf09bac19dbd526ca32a4b2e6237c4fca8
Starmy <mf99250@gmail.com>
2020-09-28 16:13:01 +0800

    Initial Commit

---
+++ HW1_00757015/HW1_00757015App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1_00757015App.swift
+//  HW1_00757015
+//
+//  Created by User19 on 2020/9/28.
+//
+
+import SwiftUI
+
+@main
+struct HW1_00757015App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
